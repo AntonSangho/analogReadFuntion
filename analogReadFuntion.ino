20322a36ad506e3a8f0f75b18081c230d705d304
Sang Ho Lee <sanghoemail@gmail.com>
2023-04-05 11:45:22 +0900

    Create analogReadFuntion.ino

---
+++ analogReadFuntion.ino
@@ -0,0 +1,27 @@
+int sens;
+int sensorPin = A0;
+
+int ReadSens_and_Condition(){
+  int i;
+  int sval = 0;
+
+  for (i = 0; i < 5; i++){
+    sval = sval + analogRead(0);    // sensor on analog pin 0
+  }
+
+  sval = sval / 5;    // average
+  sval = sval / 4;    // scale to 8 bits (0 - 255)
+  sval = 255 - sval;  // invert output
+  return sval;
+}
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  sens = ReadSens_and_Condition();
+  Serial.println(sens);
+}
+
+
